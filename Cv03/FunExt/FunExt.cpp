80b27d79a5e810278a6f2f16abef7704f47b397e
dansel71 <271457@vutbr.cz>
2024-09-30 16:01:19 +0200

    b

--- Cv03/FunExt/FunExt.cpp
+++ Cv03/FunExt/FunExt.cpp
@@ -7,18 +7,18 @@ using namespace std;
 
 int main()
 {
-    double minY = DBL_MAX;
-    int minX = INT_MAX;
-    for (int x = 1; x <= 10; ++x)
+    double maxY = DBL_MAX;
+    int maxX = INT_MAX;
+    for (int x = 10; x <= 20; ++x)
     {
         double y = 5 - 3 * x + 2 * (x - 5) * (x - 5) - (x - 10) * (x - 10) * (x - 10);
 
-        if (y < minY)
+        if (y > maxY)
         {
-            minY = y;
-            minX = x;
+            maxY = y;
+            maxX = x;
         }
     }
-    printf("Minimum value: %lf at x=%d\n", minY, minX);
+    printf("Maximum value: %lf at x=%d\n", maxY, maxX);
     return 0;
 }
