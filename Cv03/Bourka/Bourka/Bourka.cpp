599e4b3fb6db6412e649db602d3b12d07c6fc4d1
dansel71 <271457@vutbr.cz>
2024-09-30 15:15:34 +0200

    b

---
+++ Cv03/Bourka/Bourka/Bourka.cpp
@@ -0,0 +1,15 @@
+﻿// Bourka.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "Bourka.h"
+
+using namespace std;
+
+int main()
+{
+	double cas;
+	const double rychlost_zvuku = 340;
+
+	//scanf_s("Zadej cas : %fl", &cas);
+	return 0;
+}
