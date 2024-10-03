7778a1cc40c6a4163fe7f61f86eb6e263a4890b6
dansel71 <271457@vutbr.cz>
2024-10-03 13:53:38 +0200

    b

---
+++ Cv03/Bourka/Bourka/Bourka.cpp
@@ -0,0 +1,19 @@
+﻿// Bourka.cpp: Definuje vstupní bod pro aplikaci.
+//
+
+#include "Bourka.h"
+
+using namespace std;
+
+int main()
+{
+	float cas;
+	const double rychlost_zvuku = 340;
+
+	printf("Kolik sekund jsi napocital potom, co blesk uhodil?: ");
+	scanf_s("%fl", &cas);
+
+	printf("Blesk je daleko: %lf\n", cas*rychlost_zvuku);
+
+	return 0;
+}
