2f9b347dc451a5f7427a380fb3955a2fb53e7a4d
271457 <271457@vutbr.cz>
2024-10-21 15:51:16 +0200

    Cv06

--- Cv06/Počty písmen a slov/Počty písmen a slov.h
+++
@@ -1,8 +0,0 @@
-﻿// Počty písmen a slov.h: Soubor k zahrnutí pro standardní systémové soubory k zahrnutí
-// nebo soubory k zahrnutí specifické pro projekt.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Zde odkažte na dodatečné hlavičky, které program vyžaduje.
